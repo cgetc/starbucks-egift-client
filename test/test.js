2487b8e5650d0db1b1fca9932414ccac4b6cd006
komatsu <komatsu@blayn.co.jp>
2015-07-14 20:07:02 +0900

    test用のhttpサーバをnode-http-serverからconnect

--- test/test.js
+++ test/test.js
@@ -1,28 +1,30 @@
-var server=require('node-http-server');
+var url = require('url');
+var connect = require('connect');
+var serveStatic = require('serve-static');
 var assert = require("assert");
 var port = 8080;
+var hostname = process.env.SELENIUM_REMOTE_URL? url.parse(process.env.SELENIUM_REMOTE_URL).hostname : 'localhost';
+var mock_url = 'http://' + hostname + ':' + port + '/mail/';
+var app = connect();
 
-server.deploy({
-        port: port,
-        root: './test/mock/'
+app.use(serveStatic(__dirname + '/mock', {'index': ['index.html', 'index.htm']}));
+app.listen(port);
+
+var Starbucks = require('starbucks-egift-client').client({
+    payment: {
+        mail_address: 'test@test.com',
+        credit_number: '123456781234',
+        credit_month: '01',
+        credit_year: '20'
     }
-);
-
-var Starbucks = require('starbucks-egift-client').client(null, 'http://localhost:' + port + '/mail/'); // npmの読み込む
+}, mock_url); // npmの読み込む
 
 describe('create_giftcard', function () {
+    this.timeout(0);
     it('success', function (done) {
-        this.timeout(60000);
-        var form = {
-            card_message: 'test',
-            mail_address: 'test@test.com',
-            credit_number: '123456781234',
-            credit_month: '01',
-            credit_year: '20'
-        };
-        Starbucks.create_giftcard(form, function (url) {
+        Starbucks.create_giftcard('test', function (url) {
             assert.equal(url, 'http://gift.starbucks.co.jp/c/thisisdummy');
             done();
-        });
+        }, done);
     });
 });
